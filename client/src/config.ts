482129cb6c7034d05ef1bfdb179a503bd8f9a2dd
Hisham Bin Seddeq <prog.hbs@hotmail.com>
2019-12-10 22:40:12 +0300

    2nd fully working deploy,, changed node version

--- client/src/config.ts
+++ client/src/config.ts
@@ -1,5 +1,5 @@
 // TODO: Once your application is deployed, copy an API id here so that the frontend could interact with it
-const apiId = 'oudem9khij'
+const apiId = '2z7qa4kf4h'
 export const apiEndpoint = `https://${apiId}.execute-api.us-east-2.amazonaws.com/dev`
 
 export const authConfig = {
